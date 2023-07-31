9a0eb89da7984458d92eefa6913fc58801e50782
Yumi-ito1562573 <135291158+Yumi-ito1562573@users.noreply.github.com>
2023-07-31 14:21:47 +0900

    Delete gators.js

--- gators.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-// 1行目に記載している 'use strict' は削除しないでください
-
-function gator(){
-    target.textContent = "🐊";
-}
-
-const target = document.getElementById("target");
-target.addEventListener("click", gator);
