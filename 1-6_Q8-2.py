8934beb15a039c8a0fe13c3699f9cf2b9cb8bc96
Yumi-ito1562573 <135291158+Yumi-ito1562573@users.noreply.github.com>
2023-08-11 18:24:36 +0900

    Create 1-6_Q8-2.py

---
+++ 1-6_Q8-2.py
@@ -0,0 +1,25 @@
+def main():
+	group_member={'sawa':'red','tei':'black','nimiya':'blue','matsui':'pink','oshiro':'red','miyanohara':'black','furusawa':'blue'}
+	
+	'''
+	以下のような出力が得られるようなプログラムを追記してください。
+	出力の順番は問いません。
+ 
+	black = [2, ['tei', 'miyanohara']]
+	blue = [2, ['nimiya', 'furusawa']]
+	red = [2, ['oshiro', 'sawa']]
+	pink = [1, ['matsui']]
+	'''
+ 
+ 
+	color = {}
+	for name in group_member:
+		colorname = group_member[name]
+		if colorname not in color:
+			color[colorname] = [0, []]
+#ここに2行追加してください。
+ 
+	for i in color:
+		print(i, '=', color[i])
+if __name__ == '__main__':
+	main()
