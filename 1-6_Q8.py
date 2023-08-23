cc54574820fd3aa8322be3b26a54ec1d2956d3aa
Yumi-ito1562573 <135291158+Yumi-ito1562573@users.noreply.github.com>
2023-08-23 13:55:48 +0900

    Create 1-6_Q8.py

---
+++ 1-6_Q8.py
@@ -0,0 +1,18 @@
+color['blue'] = [0, []]
+color['blue'][0] = 0
+color['blue'][0] += 1
+color['blue'][0] += 1
+color['blue'][1].append('nimiya')
+color['blue'][1].append('furusawa')
+
+color['red'] = [0, []]
+color['red'][0] = 0
+color['red'][0] += 1
+color['red'][0] += 1
+color['red'][1].append('oshiro')
+color['red'][1].append('sawa')
+
+color['pink'] = [0, []]
+color['pink'][0] = 0
+color['pink'][0] += 1
+color['pink'][1].append('matsui')
